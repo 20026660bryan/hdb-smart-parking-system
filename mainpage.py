156b5af910f9868408a60c207d22ca81e7712118
20026660bryan <110346450+20026660bryan@users.noreply.github.com>
2022-08-01 05:20:57 +0800

    Update mainpage.py

--- mainpage.py
+++ mainpage.py
@@ -65,8 +65,11 @@ def load_lottieurl(url):
 #--- LOAD LOTTIE URL ---
 lottie_coding = load_lottieurl("https://assets4.lottiefiles.com/packages/lf20_swnrn2oy.json")
 
+#--- MAIN PAGE ---
 def main():
     st.set_page_config(page_title="HDB Smart Parking System", page_icon=":red_car:", layout="wide")
+    
+    #--- Hide streamlit hamburger menu ---
     hide_menu_style = """
         <style>
         #MainMenu {visibility: hidden;}
@@ -74,13 +77,15 @@ def main():
         """
     st.markdown(hide_menu_style, unsafe_allow_html=True)
 
+    #--- Sidebar containing algorithms ---
     with st.sidebar:
         with st.container():
-            st.title('Algorithms')
+            st.title('AI Algorithms')
             st.write('---')
             algorithms = ['Default(XGBoost)', 'K-Nearest Neighbours (KNN)', 'Decision Tree', 'Naive Bayes', 'Random Forest', 'Logistic Regression', 'SVM', 'MLP']
             model = st.selectbox('Select model', algorithms)
-
+            
+    #--- CONTAINER FOR INPUT ---
     with st.container():
         st.title('HDB Smart Parking System')
         st.write('---')
@@ -92,7 +97,8 @@ def main():
     with left_column:
         st.subheader('Enter the following fields')
         license = st.text_input('License Plate Number').upper()
-
+        
+        #--- Defining checksum for license plate ---
         checksum = {0: 'A', 1: 'Z', 2: 'Y', 3: 'X', 4: 'U', 5: 'T', 6: 'S', 7: 'R', 8: 'P', 9: 'M', 10: 'L',
                 11: 'K', 12: 'J', 13: 'H', 14: 'G', 15: 'E', 16: 'D', 17: 'C', 18: 'B'}
 
@@ -146,7 +152,8 @@ def main():
             if checksumvalid == 0:
                 errorplate = '<p style="font-family:sans-serif; color:Red; font-size: 12px;">Invalid license plate</p>'
                 st.markdown(errorplate, unsafe_allow_html=True)
-
+        
+        #--- Defining regex patterns for motocycle, car and goods vehicle---
         motorcycle_pattern = 'FB[^IO][1-9][0-9]{0,3}[A-E | G-H | J-M | P | R-U | X-Z]'
 
         car_pattern = 'S[B-N][^IO][1-9][0-9]{0,3}[A-E | G-H | J-M | P | R-U | X-Z]'
@@ -159,7 +166,8 @@ def main():
         nowhoursandminutes = dtnow.strftime('%H:%M')
 
         dur = st.number_input('Duration (in minutes)', min_value = 0, step = 30)
-
+        
+        #--- Validation check for duration (No need to check < 0 because user cannot input below 0) ---
         if dur > 999999:
              errordur = '<p style="font-family:sans-serif; color:Red; font-size: 12px;">Invalid duration</p>'
              st.markdown(errordur, unsafe_allow_html=True)
@@ -171,7 +179,8 @@ def main():
 
         st.info('{} hours {} minutes'.format(total_hours, total_mins))
         # st.code('{} hours {} minutes'.format(total_hours, total_mins),language="python")
-
+        
+        #--- Method to print parking expiry date based on duration input ---
         strendth = end.strftime('%H:%M ' + '| ' +  '%d' + 'th ' + '%B %Y')
         strendst = end.strftime('%H:%M ' + '| ' +  '%d' + 'st ' + '%B %Y')
         strendnd = end.strftime('%H:%M ' + '| ' +  '%d' + 'nd ' + '%B %Y')
@@ -213,7 +222,8 @@ def main():
         totalcharge = 0
         vehicle = 0
         valid = 0
-
+        
+        #--- Method to check license plate with regex ---
         if re.search(motorcycle_pattern, license):
             if checksumvalid == 1:
                 totalcharge = 0.65
@@ -244,7 +254,8 @@ def main():
 
     predict = ''
     parkingtype = ''
-
+    
+    #--- CONTAINER FOR OUTPUT---
     with st.container():
         left_column, right_column = st.columns(2)
         with left_column:
@@ -254,6 +265,8 @@ def main():
                     if st.button('Predict', disabled = False):
                         with st.spinner('Give us a moment...'):
                             time.sleep(2)
+                            
+                            #--- XGBOOST ---
                         if model == 'Default(XGBoost)':
                             predict = xgbparkingprediction(sessionstart, sessionend, totalcharge, dur, effectivecharge, vehicle)
                             if vehicle == 0:
@@ -307,7 +320,8 @@ def main():
                                     st.success('Lot Number: ' + str(shortterm))
                                     st.success('Parking fees: ' + str('$' + '{:.2f}'.format(totalcharge)))
                                     st.write("[(Learn more about parking rates)](https://www.hdb.gov.sg/car-parks/shortterm-parking/short-term-parking-charges)")
-
+                                    
+                        #--- KNN ---
                         elif model == 'K-Nearest Neighbours (KNN)':
                             predict = KNNparkingprediction(sessionstart, sessionend, totalcharge, dur, effectivecharge, vehicle)
                             if vehicle == 0:
@@ -362,7 +376,7 @@ def main():
                                     st.success('Parking fees: ' + str('$' + '{:.2f}'.format(totalcharge)))
                                     st.write("[(Learn more about parking rates)](https://www.hdb.gov.sg/car-parks/shortterm-parking/short-term-parking-charges)")
 
-
+                        #--- DECISION TREE ---
                         elif model == 'Decision Tree':
                             predict = dtparkingprediction(sessionstart, sessionend, totalcharge, dur, effectivecharge, vehicle)
                             if vehicle == 0:
@@ -416,7 +430,8 @@ def main():
                                     st.success('Lot Number: ' + str(shortterm))
                                     st.success('Parking fees: ' + str('$' + '{:.2f}'.format(totalcharge)))
                                     st.write("[(Learn more about parking rates)](https://www.hdb.gov.sg/car-parks/shortterm-parking/short-term-parking-charges)")
-
+ 
+                        #--- NAIVE BAYES ---
                         elif model == 'Naive Bayes':
                             predict = nbparkingprediction(sessionstart, sessionend, totalcharge, dur, effectivecharge, vehicle)
                             if vehicle == 0:
@@ -470,7 +485,8 @@ def main():
                                     st.success('Lot Number: ' + str(shortterm))
                                     st.success('Parking fees: ' + str('$' + '{:.2f}'.format(totalcharge)))
                                     st.write("[(Learn more about parking rates)](https://www.hdb.gov.sg/car-parks/shortterm-parking/short-term-parking-charges)")
-
+                        
+                        #--- RANDOM FOREST ---
                         elif model == 'Random Forest':
                             predict = rfparkingprediction(sessionstart, sessionend, totalcharge, dur, effectivecharge, vehicle)
                             if vehicle == 0:
@@ -524,7 +540,8 @@ def main():
                                     st.success('Lot Number: ' + str(shortterm))
                                     st.success('Parking fees: ' + str('$' + '{:.2f}'.format(totalcharge)))
                                     st.write("[(Learn more about parking rates)](https://www.hdb.gov.sg/car-parks/shortterm-parking/short-term-parking-charges)")
-
+                        
+                        #--- LOGISTIC REGRESSION ---
                         elif model == 'Logistic Regression':
                             predict = lrparkingprediction(sessionstart, sessionend, totalcharge, dur, effectivecharge, vehicle)
                             if vehicle == 0:
@@ -578,7 +595,8 @@ def main():
                                     st.success('Lot Number: ' + str(shortterm))
                                     st.success('Parking fees: ' + str('$' + '{:.2f}'.format(totalcharge)))
                                     st.write("[(Learn more about parking rates)](https://www.hdb.gov.sg/car-parks/shortterm-parking/short-term-parking-charges)")
-
+                                    
+                        #--- SVM ---
                         elif model == 'SVM':
                             predict = svmparkingprediction(sessionstart, sessionend, totalcharge, dur, effectivecharge, vehicle)
                             if vehicle == 0:
@@ -632,7 +650,8 @@ def main():
                                     st.success('Lot Number: ' + str(shortterm))
                                     st.success('Parking fees: ' + str('$' + '{:.2f}'.format(totalcharge)))
                                     st.write("[(Learn more about parking rates)](https://www.hdb.gov.sg/car-parks/shortterm-parking/short-term-parking-charges)")
-
+                        
+                        #--- MLP ---
                         elif model == 'MLP':
                             predict = mlpparkingprediction(sessionstart, sessionend, totalcharge, dur, effectivecharge, vehicle)
                             if vehicle == 0:
