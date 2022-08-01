f9cb25267d049b33692f5110eb0f030146350344
She-Jun-Heng <87625401+20018792-She-JunHeng@users.noreply.github.com>
2022-08-01 12:05:42 +0800

    Update mainpage.py

--- mainpage.py
+++ mainpage.py
@@ -171,7 +171,6 @@ def main():
         if dur > 999999:
              errordur = '<p style="font-family:sans-serif; color:Red; font-size: 12px;">Invalid duration</p>'
              st.markdown(errordur, unsafe_allow_html=True)
-             st.code('')
 
         #else:
         end = dtnow + timedelta(minutes = dur)
