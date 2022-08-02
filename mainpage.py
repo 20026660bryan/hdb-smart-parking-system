d06d0c506e60c50b3a1384d708901d2db9ff9a5e
20026660bryan <110346450+20026660bryan@users.noreply.github.com>
2022-08-02 10:49:14 +0800

    Update mainpage.py

--- mainpage.py
+++ mainpage.py
@@ -13,6 +13,8 @@ from streamlit_lottie import st_lottie
 
 #--- LOAD MODELS ---
 
+@st.cache
+
 KNN = pickle.load(open('PKL_files/tunedKNN.pkl', 'rb'))
 
 DecisionTree = pickle.load(open('PKL_files/tunedDecisionTree.pkl', 'rb'))
